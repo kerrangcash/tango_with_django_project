0226c2aa79a683c0554982b16ff4634191f5a915
Ben Lynch <2381564l@student.gla.ac.uk>
2020-02-13 13:52:56 +0000

    Chapter 6

--- rango/views.py
+++ rango/views.py
@@ -4,11 +4,43 @@ from django.http import HttpResponse
 
 from django.shortcuts import render
 
+from rango.models import Page
+
+#import category model
+from rango.models import Category
+
 def index(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
+    
+    #render the response and send it back
     return render(request, 'rango/index.html', context=context_dict)
     
 def about(request):
     context_dict = {'boldmessage': 'This tutorial has been put together by Ben'}
     return render(request, 'rango/about.html', context=context_dict)
 
+def show_category(request, category_name_slug):
+    context_dict = {}
+    
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        
+        pages = Page.objects.filter(category=category)
+        
+        context_dict['pages'] = pages
+        
+        context_dict['category'] = category
+        
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+        
+        
+    return render(request, 'rango/category.html', context=context_dict)
